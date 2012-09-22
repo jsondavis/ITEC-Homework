e9bf63ecb203c089b8a63831dc0752616dc1e328
Jason Davis <electrode05@gmail.com>
2012-09-21 22:39:15 -0400

    "fixed MilitaryTime's calcs and Letters' spacing"

--- homework4/letters/LetterH.java
+++ homework4/letters/LetterH.java
@@ -1,17 +1,14 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package homework4.letters;
 
 /**
- *
- * @author JDavis
- */
+*
+* @author JDavis
+*/
 public class LetterH {
-    private static String[] lines = {"*    *", "*    *", " **** ", "*    *", "*    *"};
- 
-   
+    private static String[] lines = {"*\u0020\u0020\u0020\u0020*", "*\u0020\u0020\u0020\u0020*"
+            , "\u0020****\u0020", "*\u0020\u0020\u0020\u0020*", "*\u0020\u0020\u0020\u0020*"};
+
+
     public static String toString(int line){
         return lines[line];
     }
